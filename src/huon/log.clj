3523b41d9efb1e5e792ac24c77b821328a2ec04f
Gert Goet <gert@thinkcreate.nl>
2018-09-01 18:52:21 +0200

    Show linenumber besides namespace

--- src/huon/log.clj
+++ src/huon/log.clj
@@ -1,27 +1,33 @@
 (ns huon.log)
 
 (defmacro ^:private log [level args]
-  `(log* ~(str *ns*)
-         ~level
-         #(str "[" (clojure.string/upper-case (name ~level))  "] "
-               (clojure.string/join " " ~(mapv (fn [x] `(str ~x)) args)))))
+  (let [origin-meta# (-> &form meta :origin)
+        origin#      (str (:ns origin-meta#) ":" (:line origin-meta#))]
+    `(log* ~(str *ns*)
+           ~level
+           #(str "[" ~origin# "] "
+                 "[" (clojure.string/upper-case (name ~level))  "] "
+                 (clojure.string/join " " ~(mapv (fn [x] `(str ~x)) args))))))
+
+(defn- origin-meta [form]
+  {:origin {:ns *ns* :line (:line (meta form))}})
 
 (defmacro debug
   "Evaluate and log args if level >= :debug"
   [& args]
-  `(log :debug ~args))
+  (with-meta `(log :debug ~args) (origin-meta &form)))
 
 (defmacro info
   "Evaluate and log args if level >= :info"
   [& args]
-  `(log :info ~args))
+  (with-meta `(log :info ~args) (origin-meta &form)))
 
 (defmacro warn
   "Evaluate and log args if level >= :warn"
   [& args]
-  `(log :warn ~args))
+  (with-meta `(log :warn ~args) (origin-meta &form)))
 
 (defmacro error
   "Evaluate and log args if level >= :error"
   [& args]
-  `(log :error ~args))
+  (with-meta `(log :error ~args) (origin-meta &form)))
